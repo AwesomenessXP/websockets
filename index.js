306b4b512b538686ab09e083d1a2dd088566841c
HaskMoney <heartstring85@gmail.com>
2022-12-10 15:38:49 -0800

    added event that takes user input

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// make http server listen on port 3000
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
 
 // define route handler '/' that's called when we hit home page
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html'); 
 });
 
-// make http server listen on port 3000
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-})
+io.on('connection', (socket) => {
+  console.log('a user connected!!!');
+  socket.on('disconnect', () => {
+    console.log('user disconnected :(');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat msg', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
